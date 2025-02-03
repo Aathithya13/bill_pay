a9c369841e6c136ccbb8239a052c36e0dd3058dc
Anshita100 <anshitagupta2277@gmail.com>
2025-02-03 22:39:33 +0530

    updated global exception handler

--- CAC/src/main/java/com/example/exception/GlobalExceptionHandler.java
+++ CAC/src/main/java/com/example/exception/GlobalExceptionHandler.java
@@ -18,20 +18,10 @@ import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-  /*  @ExceptionHandler(BillNotFoundException.class)
-    public ResponseEntity<String> handlePaymentNotFoundException(BillNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
-    }*/
-	
-	/*@ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleEmployeeNotFoundException(ResourceNotFoundException ex) {
-        Map<String, String> error = new HashMap<>();
-        error.put("message", ex.getMessage());
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }*/
+  
 	@ExceptionHandler(BillNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleResourceNotFound1(BillNotFoundException ex) {
-		System.out.println("Anshita");
+		System.out.println("bill not found exception");
         ErrorResponse response = new ErrorResponse(
             ex.getMessage(),
             HttpStatus.NOT_FOUND.value(),
@@ -41,8 +31,8 @@ public class GlobalExceptionHandler {
     }
 	@ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleResourceNotFound2(IllegalArgumentException ex) {
-		System.out.println("Anshita 1");
-        ErrorResponse response = new ErrorResponse(
+		System.out.println("IllegalArgumentException exception");
+		        ErrorResponse response = new ErrorResponse(
             ex.getMessage(),
             HttpStatus.BAD_REQUEST.value(),
             LocalDateTime.now().toString()
@@ -52,7 +42,7 @@ public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(NullPointerException.class)
     public ResponseEntity<ErrorResponse> handleResourceNotFound3(NullPointerException ex) {
-		System.out.println("Anshita 2");
+		System.out.println("Null pointer exception");
         ErrorResponse response = new ErrorResponse(
             ex.getMessage(),
             HttpStatus.INTERNAL_SERVER_ERROR.value(),
@@ -66,22 +56,7 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>("An error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-	/*@ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleResourceNotFound3(ResourceNotFoundException ex) {
-		System.out.println("Anshita gupta");
-        ErrorResponse response = new ErrorResponse(
-            ex.getMessage(),
-            HttpStatus.INTERNAL_SERVER_ERROR.value(),
-            LocalDateTime.now().toString()
-        );
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-    }*/
-
-	  /*  @ExceptionHandler(ResourceNotFoundException.class)
-	    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
-	        return new ResponseEntity<>("Resource not found: " + ex.getMessage(), HttpStatus.NOT_FOUND);
-	    }
-*/
+	
 	   
   
     
